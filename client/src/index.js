0980ba504ea8ad7a2dbd727c209c086fc00d20e5
Luis Daniel Rios <luisdanielrios@comuna18.com>
2022-08-31 22:51:05 -0500

    Countries Post Deploy 2.6

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import store from "./store";
 import dotenv from "dotenv";
 dotenv.config();
 
-axios.defaults.baseURL = process.env.REAC_APP_API || "http://localhost:3001";
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
